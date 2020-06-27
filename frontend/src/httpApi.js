af6e1bff591305450c3c8ea4b411391649911bf2
Arthelon <hsing.daniel@gmail.com>
2020-06-27 12:45:59 +0800

    Add env variable to configure API url
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- frontend/src/httpApi.js
+++ frontend/src/httpApi.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-export const callExternalEndpoint = async token =>
-    axios.get(`http://localhost:3001/api/external`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001 ";
+
+export const callExternalEndpoint = async (token) =>
+  axios.get(`${API_URL}/api/external`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
